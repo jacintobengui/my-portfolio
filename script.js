ca5a30023bcd8aec190ce1280419d719c2a57bed
Jacinto Bengui <gersonbengui2006@gmail.com>
2025-07-29 18:56:05 +0100

    trabalhando no sobre: preenchendo infos 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var mobile_button = document.getElementById('mobile_btn')
 var mobile_menu = document.getElementById('mobile_menu')
-
+var nav_items = document.querySelectorAll('.nav-item')
 
 mobile_button.onclick = () => {
     mobile_menu.classList.toggle('active')
@@ -10,4 +10,13 @@ mobile_button.onclick = () => {
         mobile_button.style.background = `url('../img/icons8-menu.svg') no-repeat center center`
     }
 }
+
+nav_items.forEach(item => {
+    item.onclick = () => {
+        mobile_menu.classList.remove('active')
+        mobile_button.style.background = `url('../img/icons8-menu.svg') no-repeat center center`
+    }
+})
+
+
 
